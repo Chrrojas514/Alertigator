021c74f7f67757e60bdd20813be06a0dc887bb0b
unknown <chrisrojas514@gmail.com>
2023-10-08 18:35:09 -0400

    fixed some of the remaining lint issues

--- commandsHandler.js
+++ commandsHandler.js
@@ -1,14 +1,16 @@
 const { REST } = require('@discordjs/rest');
-const { Routes, Client, Message } = require('discord.js');
-const utils = require(`./utilities/utilityFunctions`);
+const { Routes } = require('discord.js');
+
+const utils = require('./utilities/utilityFunctions');
 require('dotenv/config');
 
-let commands = [];
-const commandFiles = utils.getFiles(`./commands`);
+const commands = [];
+const commandFiles = utils.getFiles('./commands');
 
 for (const file of commandFiles) {
-    const command = require(file);
-    commands.push(command.data.toJSON())
+  // eslint-disable-next-line global-require
+  const command = require(file);
+  commands.push(command.data.toJSON());
 }
 
 console.log(commands);
@@ -16,19 +18,18 @@ console.log(commands);
 const rest = new REST().setToken(process.env.TOKEN);
 
 (async () => {
+  console.log(`Refreshing ${commands.length} slash commands...`);
 
-    console.log(`Refreshing ${commands.length} slash commands...`);
+  try {
+    const data = await rest.put(
+      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+      { body: commands },
+    );
 
-    try {
-        const data = await rest.put(
-            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
-            { body : commands},
-        );
+    console.log(data);
 
-        console.log(data);
-
-        console.log(`Successfully refreshed ${commands.length} slash commands!`);
-    } catch (error) {
-        console.log(error);
-    }
+    console.log(`Successfully refreshed ${commands.length} slash commands!`);
+  } catch (error) {
+    console.log(error);
+  }
 })();
