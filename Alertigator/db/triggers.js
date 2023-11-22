b196cb8cbd4e98c940dcd1dda50f87475f32306a
Raymond Xu <59592544+raymondxu5866@users.noreply.github.com>
2023-11-22 03:13:00 -0500

    Update triggers.js
    reminder now pings users

--- Alertigator/db/triggers.js
+++ Alertigator/db/triggers.js
@@ -17,18 +17,20 @@ const checkForReminders = async () => {
     // parse reminder model
     const reminderData = JSON.stringify(existingReminder);
 
-    // again, this should be its own utility function or file in utilities
-    const returnEmbed = new EmbedBuilder()
+    // file in utilities
+    /*
+      const returnEmbed = new EmbedBuilder()
       .setColor(0xdeffe7)
-      .setDescription(`<@${existingReminder.user_id}> Reminder! ${existingReminder.message}`);
+      .setDescription(`${existingReminder.message}`);
+    */
 
-    // Hard-coded the guild id from our server, refactor to represent any guild id from the
     // retrieved reminder
     client.channels.fetch('1147987485469724733').then((channel) => {
-      channel.send({ embeds: [returnEmbed] });
+      channel.send(`<@${existingReminder.user_id}> Reminder: ${existingReminder.message}`);
     });
 
     console.log(`Reminder delivered: ${reminderData}`);
+    
 
     await existingReminder.destroy();
   }
