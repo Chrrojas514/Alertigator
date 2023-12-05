d70615264298e2531f991093e2c8431afc8c9b5a
elulim <56937083+elulim@users.noreply.github.com>
2023-12-04 21:15:43 -0500

    Add files via upload
    added variables for News & conversion models

--- Alertigator/utilities/updateDatabase.js
+++ Alertigator/utilities/updateDatabase.js
@@ -1,3 +1,8 @@
 const Reminder = require('../models/reminder');
+const DailyNews = require('../models/News');
+const CurrencyConversion = require('../models/conversion');
 
+// Sync the models to generate tables in the database
+DailyNews.sync({ force: true }); 
+CurrencyConversion.sync({ force: true }); 
 Reminder.sync({ force: true }); // WARNING - CLEARS EVERYTHING CURRENTLY IN DB
